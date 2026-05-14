f4390e011831a8eb1da87295be5ce55a2599c597
eplamondon2 <eplamondon@hyundaistraymond.ca>
2026-05-14 10:39:41 -0400

    Update email.js

--- src/services/email.js
+++ src/services/email.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer').default || require('nodemailer');
 
 const transporter = nodemailer.createTransporter({
   host: process.env.SMTP_HOST,
